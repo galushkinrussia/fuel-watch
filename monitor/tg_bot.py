9fd94d5c8c28aa7d4bf8b7b01c0782bcde99d434
Mike Galushkin <mgalushkin@mail.ru>
2026-09-13 19:55:51 +0300

    docs: 'Приглашаю в бота' в инвайте

--- monitor/tg_bot.py
+++ monitor/tg_bot.py
@@ -459,7 +459,7 @@ def handle_command(text, chat_id, user_id, username, admins, token, data, users_
         if not admin:
             return "Недостаточно прав."
         code = new_invite(data)
-        head = (f"Привет!\nАдрес бота: @{BOT_USERNAME} "
+        head = (f"Привет!\nПриглашаю в бота: @{BOT_USERNAME} "
                 f"(https://t.me/{BOT_USERNAME})\n\n")
         return (head + f"Активируй приглашение командой:\n<code>/invite {code}</code>",
                 "HTML")
